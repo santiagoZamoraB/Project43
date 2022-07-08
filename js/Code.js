e270b303aa86b20f8b711ff52befd19ece55d62d
santiagoZamoraB <83790163+santiagoZamoraB@users.noreply.github.com>
2022-07-07 19:40:27 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+  fill("white")
+  textSize(15)
+  text("REVBAILA", 100, 50)
+  text("CUNIFON", 700, 150)
+  text("ATEDASBA ", 100, 325)
+  fill("lightblue")
+  text("Hint: Always changin, not constant !!", 100, 70)
+  text("Hint: Performs a particular task !!",700, 175)
+  text("Hint: Stores all the information !!", 100, 350)
+    
+
+}
